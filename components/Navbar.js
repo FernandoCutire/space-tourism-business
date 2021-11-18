570072db9bbc6827a619538949ee2d31aaa0e877
Fernando Cutire ubuntu <fernandocutire@gmail.com>
2021-11-17 20:57:03 -0500

    PROGRESS: Avance en sass - progreso en la pagina landing

--- components/Navbar.js
+++ components/Navbar.js
@@ -32,3 +32,5 @@ function Navbar() {
     </div>
   );
 }
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.sass';
+// import '../styles/Home.module.sass';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Apps with Fercu and Asley</title>
         <meta
@@ -14,21 +15,23 @@ export default function Home() {
         <link rel='icon' href='/favicon-32x32.png' />
       </Head>
 
-      <main className={styles.main}>
-        <section>
-          <article>
-            <h2>So, you want to go to space</h2>
-            <h1>Space</h1>
-            <p>
+      <main className='home'>
+        <section className='home__main'>
+          <article className='home__text--container'>
+            <h2 className='home--subheading'>So, you want to go to space</h2>
+            <h1 className='home--heading'>Space</h1>
+            <p className='home--para'>
               Let’s face it; if you want to go to space, you might as well
               genuinely go to outer space and not hover kind of on the edge of
               it. Well sit back, and relax because we’ll give you a truly out of
               this world experience!
             </p>
           </article>
-          <button>
-            <span>Explore</span>
-          </button>
+          <Link href='/destination' passHref>
+            <button className='home__explore--container' aria-expanded='false'>
+              <span>Explore</span>
+            </button>
+          </Link>
         </section>
       </main>
     </div>
